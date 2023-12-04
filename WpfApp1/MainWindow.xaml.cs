c40f31d8116d81fe0c9846cba32623d4ef2007fc
vovako <vladimirkondaurov38@gmail.com>
2023-12-05 00:45:24 +0500

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random _random = new Random();
+        private double _threshold = 5; // Пороговая температура для уведомления
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Генерация рандомных температур каждые 5 секунд
+            var temperatureObservable = Observable.Interval(TimeSpan.FromSeconds(5))
+                .Select(_ => _random.Next(-10, 30));
+
+            // Подписка на изменение температуры
+            temperatureObservable.Subscribe(temperature =>
+            {
+                // Обновление отображаемой температуры
+                Dispatcher.Invoke(() => TemperatureLabel.Content = $"{temperature}°C");
+
+                // Проверка на приближение к критической температуре и отправка уведомления
+                if (temperature <= _threshold)
+                {
+                    Dispatcher.Invoke(() => ShowCriticalTemperatureNotification());
+                }
+            });
+        }
+
+        // Метод для отображения уведомления о критической температуре
+        private void ShowCriticalTemperatureNotification()
+        {
+            MessageBox.Show("Температура приближается к критической!", "Внимание", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+}
